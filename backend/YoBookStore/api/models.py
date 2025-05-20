a31382ca197799c44426a60c451642240336c30f
sekibaalamark <markmajje256@gmail.com>
2025-05-20 20:11:46 +0300

    created a basic model model

--- backend/YoBookStore/api/models.py
+++ backend/YoBookStore/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    pdf_file = models.FileField(upload_to='books/')
+
+    def __str__(self):
+        return self.title
--- backend/YoBookStore/api/views.py
+++ backend/YoBookStore/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
 
 # Create your views here.
